98118661193298b911cf494a5abc30d370eed766
Jay B <79182784+JayBee-K@users.noreply.github.com>
2022-02-17 20:47:46 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,22 @@
 let windowWidth = $(window).width();
 const handleTouchMove = function (ev) {
-	ev.preventDefault();
+    ev.preventDefault();
+}
+const slideBanner = function () {
+    new Swiper('#slideBanner', {
+        loop: false,
+        speed: 500,
+        navigation: {
+            nextEl: '#slideBanner .slide-button-next',
+            prevEl: '#slideBanner .slide-button-prev',
+        },
+        autoplay: {
+            delay: 10000,
+            disableOnInteraction: false,
+        },
+    });
 }
 
-
 $(function () {
+    slideBanner();
 });
