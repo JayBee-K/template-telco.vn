f38a0c5fbbc828acfd5b4dd8b098aa1a6f5154e5
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2022-02-18 17:22:47 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,83 @@
 let windowWidth = $(window).width();
 const handleTouchMove = function (ev) {
-    ev.preventDefault();
+	ev.preventDefault();
 }
+const navigationMobile = function (e) {
+	if (windowWidth < 992) {
+		$("#header .header-inner .header-inner_nav > ul > li > ul").each(function (index) {
+			$(this).prev().attr({
+				"href": "#subMenuTopUp" + index,
+				"data-toggle": "collapse"
+			});
+
+			$(this).prev().find('i').remove();
+			$(this).prev().append(`<span><i class="fal fa-plus"></i></span>`);
+			$(this).attr({
+				"id": "subMenuTopUp" + index,
+				"class": "collapse list-unstyled mb-0",
+				"data-parent": "#hasMenuTopUp"
+			});
+		})
+
+		/*
+		 * Call menu mobile
+		 */
+		let body = $('body'),
+			hamburgerIcon = $('#call-header_mobile');
+
+		hamburgerIcon.click(function (e) {
+			if (!body.hasClass('is-show_navigation')) {
+				body.attr({
+					'class': 'is-show_navigation',
+					'style': 'overflow-y: hidden'
+				});
+				document.addEventListener('touchmove', handleTouchMove, {passive: false});
+				$('#user-mobile').removeClass('active');
+			} else {
+				body.attr({
+					'class': '',
+					'style': ''
+				});
+				document.removeEventListener('touchmove', handleTouchMove);
+			}
+		});
+
+		$('#header-overlay').click(function () {
+			$('body').attr({
+				'class': '',
+				'style': ''
+			});
+			document.removeEventListener('touchmove', handleTouchMove);
+		});
+	}
+}
+const headerScroll = function (e) {
+	$(window).scroll(function (e) {
+		if ($(document).scrollTop() > $('#header').innerHeight()) {
+			$('#header').addClass('is-scroll').removeClass('is-scrolled');
+		} else {
+			$('#header').removeClass('is-scroll').addClass('is-scrolled');
+		}
+	});
+}
+
 const slideBanner = function () {
-    new Swiper('#slideBanner', {
-        loop: false,
-        speed: 500,
-        navigation: {
-            nextEl: '#slideBanner .slide-button-next',
-            prevEl: '#slideBanner .slide-button-prev',
-        },
-        autoplay: {
-            delay: 10000,
-            disableOnInteraction: false,
-        },
-    });
+	new Swiper('#slideBanner', {
+		loop: false,
+		speed: 500,
+		navigation: {
+			nextEl: '#slideBanner .slide-button-next',
+			prevEl: '#slideBanner .slide-button-prev',
+		},
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+		},
+	});
 }
 
 $(function () {
-    slideBanner();
+	headerScroll();
+	slideBanner();
+	navigationMobile();
 });
